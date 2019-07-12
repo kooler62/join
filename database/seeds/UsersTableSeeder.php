ece79fba3e93055a43e1fc5497dcb579a821742f
kooler62 <kooler62@gmail.com>
2019-07-12 17:58:58 +0300

    Use database seeds to create first user with email admin@admin.com and password “password”

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::insert([
+            [
+                'name' => 'admin',
+                'email' => 'admin@admin.com',
+                'password' => bcrypt('password'),
+                'is_admin' => true,
+            ],
+        ]);
+    }
+}
