0e5b4312996806d4d57895c89cd4ca0694386adb
kooler62 <kooler62@gmail.com>
2019-07-12 18:15:38 +0300

    Add Company entity

---
+++ database/factories/CompanyFactory.php
@@ -0,0 +1,15 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Company;
+use Faker\Generator as Faker;
+
+$factory->define(Company::class, function (Faker $faker) {
+    return [
+        'name' => $faker->company(),
+        'email' => $faker->email(),
+        'logo' => $faker->imageUrl(100, 100),
+        'website' => $faker->url(),
+    ];
+});
